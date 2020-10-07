bc37517925c3b1b19d819d5f23727c861727efc7
Sebastian Ballesteros Ramirez <sballesteros@Strelitzia.local>
2020-10-06 21:40:57 -0400

    Added cross-validation and optargs

--- a1.py
+++ a1.py
@@ -6,6 +6,7 @@ Sebastian Ballesteros
 '''
 
 ############################## IMPORTS  ########################################
+
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -14,10 +15,9 @@ from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
+from sklearn.model_selection import ShuffleSplit
 
-import sys
 import nltk
-import numpy as np
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -25,26 +25,36 @@ from nltk.stem import WordNetLemmatizer
 #nltk.download('punkt')
 #nltk.download('stopwords')
 from nltk.corpus import stopwords
-
 from nltk import word_tokenize
 
 from matplotlib import pyplot as plt
 import math
 import pandas
 import codecs
+import numpy as np
 import string
+import sys
+import argparse
+
+########################### GLOBAL VARIABLES ##################################
+
+lemmatize = False
+smoothing = 1
+stem = False
+min_frequency = 2
+train_percentage = 0.8
+test_percentage = 0.2
+cross_val_splits = 10
+verbose = False
 
 ########################### GLOBAL CONSTANTS ##################################
-LEMMATIZE = False
-SMOOTHING = 1
-STEM = False
-MIN_FREQUENCY = 2
-TRAIN_PERCENTAGE = 0.9
-TEST_PERCENTAGE = 0.1
+
 SENTENCES_COUNT = 10662
+NUM_MODELS = 5
 DATA_DIRECTORY = r'./rt-polaritydata'
 
 ###########################  HELPER FUNCTIONS ##################################
+
 def read_file(filename):
     with codecs.open(filename, 'r', encoding='utf-8', errors='ignore') as file:
              return file.readlines()
@@ -55,7 +65,7 @@ def tokenize(sentence):
     return sentence_tokenized
 
 def stem(sentences):
-    stemmer = PorterStemmer()
+    stemmer = Porterstemmer()
 
     sentences_stemmed = []
     for sentence in sentences:
@@ -63,7 +73,7 @@ def stem(sentences):
     return sentences_stemmed
 
 def lemmatize(sentences):
-    lemmatizer = WordNetLemmatizer()
+    lemmatizer = WordNetlemmatizer()
 
     sentences_lemmatized = []
     for sentence in sentences:
@@ -79,6 +89,55 @@ def remove_stop_words(tokenized_sentencees):
         new_tokenized_sentences.append(without_stop_words)
     return new_tokenized_sentences
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l', action='store_true', dest='lemmatize', default=False,
+                    help='Lemmatize in preprocess')
+
+    parser.add_argument('-s', action='store_true', dest='stem', default=False,
+                    help='Stem in preprocess')
+
+    parser.add_argument('-tr', action='store', default=0.8,
+                    dest='train_percentage',
+                    help='Set the train percentage')
+
+    parser.add_argument('-te', action='store', default=0.2,
+                        dest='test_percentage',
+                        help='Set the train percentage')
+
+    parser.add_argument('-sm', action='store', default=1,
+                    dest='smoothing',
+                    help='Set the smoothing value')
+
+    parser.add_argument('-minf', action='store', default=1,
+                    dest='min_f',
+                    help='Set the minimum frequency for features')
+
+    parser.add_argument('-cross', action='store', default=10,
+                    dest='cross_val',
+                    help='Set the cross validation split iterations')
+
+    parser.add_argument('-v', action='store_true', dest='verbose', default=False,
+                        help='Print the confusion matrix of each iteration')
+
+    results = parser.parse_args()
+    global lemmatize
+    lemmatize = results.lemmatize
+    global smoothing
+    smoothing  = results.smoothing
+    global stem
+    stem = results.stem
+    global min_frequency
+    min_frequency = results.min_f
+    global train_percentage
+    train_percentage = results.train_percentage
+    global test_percentage
+    test_percentage = results.test_percentage
+    global cross_val_splits
+    cross_val_splits = results.cross_val
+    global verbose
+    verbose = results.verbose
 
 ###########################  MAIN FUNCTIONS ##################################
 
@@ -95,19 +154,19 @@ def load_data():
     return(x, y)
 
 
-def preprocess(x, y):
+def preprocess(x, y, train_index, test_index):
 
     #lemmatize or stem
-    if(LEMMATIZE):
+    if(lemmatize):
         x = lemmatize(x)
-    elif(STEM):
+    elif(stem):
         x = stem(x)
 
-    #Separate dataset
-    x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=TEST_PERCENTAGE)
+    x_train, x_test = x[train_index], x[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 
     #extract features and remove infrequently occurring words
-    tokenizer = CountVectorizer(min_df=MIN_FREQUENCY, tokenizer=nltk.word_tokenize, stop_words='english')
+    tokenizer = CountVectorizer(min_df=min_frequency, tokenizer=nltk.word_tokenize, stop_words='english')
     x_counts = tokenizer.fit_transform(x_train)
 
     # Convert raw frequency counts into TF-IDF (Term Frequency -- Inverse Document Frequency) values
@@ -122,7 +181,7 @@ def preprocess(x, y):
 
 
 def train_naive_bayes(x_train, y_train, x_test):
-    naive_bayes = MultinomialNB(alpha=SMOOTHING)
+    naive_bayes = MultinomialNB(alpha=smoothing)
 
     naive_bayes.fit(x_train, y_train.ravel())
 
@@ -134,9 +193,10 @@ def train_naive_bayes(x_train, y_train, x_test):
 
 def test_naive_bayes(y_test, y_pred):
     accuracy = accuracy_score(y_test, y_pred) * 100
-    print('-----------Naive Bayes-------------')
-    print("Accuracy: {:.2f}%".format(accuracy))
-    print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
+    if(verbose):
+        print('-----------Naive Bayes-------------')
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
     return accuracy
 
 def train_logistic_regression(x_train, y_train, x_test):
@@ -152,9 +212,10 @@ def train_logistic_regression(x_train, y_train, x_test):
 
 def test_logistic_regression(y_test, y_pred):
     accuracy = accuracy_score(y_test, y_pred) * 100
-    print('-------Logistic Regression--------')
-    print("Accuracy: {:.2f}%".format(accuracy))
-    print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
+    if(verbose):
+        print('-------Logistic Regression--------')
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
     return accuracy
 
 def train_support_vector(x_train, y_train, x_test):
@@ -170,9 +231,10 @@ def train_support_vector(x_train, y_train, x_test):
 
 def test_support_vector(y_test, y_pred):
     accuracy = accuracy_score(y_test, y_pred) * 100
-    print('-----Support Vector Machines------')
-    print("Accuracy: {:.2f}%".format(accuracy))
-    print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
+    if(verbose):
+        print('-----Support Vector Machines------')
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
     return accuracy
 
 def train_bernoulli(x_train, y_train, x_test):
@@ -188,20 +250,22 @@ def train_bernoulli(x_train, y_train, x_test):
 
 def test_bernoulli(y_test, y_pred):
     accuracy = accuracy_score(y_test, y_pred) * 100
-    print('-------------Bernoulli------------')
-    print("Accuracy: {:.2f}%".format(accuracy))
-    print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
+    if(verbose):
+        print('-------------Bernoulli------------')
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
     return accuracy
 
 def train_random():
-    y_pred = np.random.randint(2, size=(math.ceil(SENTENCES_COUNT * TEST_PERCENTAGE)))
+    y_pred = np.random.randint(2, size=(math.ceil(SENTENCES_COUNT * test_percentage)))
     return y_pred
 
 def test_random(y_test, y_pred):
     accuracy = accuracy_score(y_test, y_pred) * 100
-    print('---------------Random-------------')
-    print("Accuracy: {:.2f}%".format(accuracy))
-    print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
+    if(verbose):
+        print('---------------Random-------------')
+        print("Accuracy: {:.2f}%".format(accuracy))
+        print("Confusion Matrix: \n {}".format(confusion_matrix(y_test, y_pred)))
     return accuracy
 
 def plot_results(models_accuracy):
@@ -214,11 +278,12 @@ def plot_results(models_accuracy):
     # Place values on top of bars
     for i, v in enumerate(models_accuracy):
         #(x, y, text)
+
         ax1.text(i - 0.25, 40, v[0])
         ax1.text(i - 0.25, v[1] + 2, str(v[1]) + '%')
 
-    ax1.set_ylabel('Accuracy (%)')
-    ax1.set_title('Models')
+    ax1.set_ylabel('Average accuracy (%)')
+    ax1.set_title('Models ran in {} iteration'.format(cross_val_splits))
     ax1.set_ylim([0, 100])
 
     plt.show()
@@ -226,31 +291,51 @@ def plot_results(models_accuracy):
 def main():
     x, y = load_data()
 
-    x_train, x_test, y_train, y_test = preprocess(x, y)
+    #Run cross validation
+    cross_val = ShuffleSplit(n_splits=cross_val_splits, test_size=test_percentage)
 
+    naive_bayes_accuracy = []
+    lregression_accuracy = []
+    svector_accuracy = []
+    bernoulli_accuracy = []
+    random_accuracy = []
+
+    #Preprocess, train and test for each model with all the cross-val splits
+    for train_index, test_index in cross_val.split(x):
+
+        x_train, x_test, y_train, y_test = preprocess(x, y, train_index, test_index)
+
+        y_pred = train_naive_bayes(x_train, y_train, x_test)
+        accuracy =test_naive_bayes(y_test, y_pred)
+        naive_bayes_accuracy.append(accuracy)
+
+        y_pred = train_logistic_regression(x_train, y_train, x_test)
+        accuracy = test_logistic_regression(y_test, y_pred)
+        lregression_accuracy.append(accuracy)
+
+        y_pred = train_support_vector(x_train, y_train, x_test)
+        accuracy = test_support_vector(y_test, y_pred)
+        svector_accuracy.append(accuracy)
+
+        y_pred = train_bernoulli(x_train, y_train, x_test)
+        accuracy = test_bernoulli(y_test, y_pred)
+        bernoulli_accuracy.append(accuracy)
+
+        y_pred = train_random()
+        accuracy = test_random(y_test, y_pred)
+        random_accuracy.append(accuracy)
+
+    # Compute average of all runs
     models_accuracy = []
+    models_accuracy.append(('Naive Bayes', sum(naive_bayes_accuracy)/len(naive_bayes_accuracy)))
+    models_accuracy.append(('Logistic Regression', sum(lregression_accuracy)/len(lregression_accuracy)))
+    models_accuracy.append(('Support Vector', sum(svector_accuracy)/len(svector_accuracy)))
+    models_accuracy.append(('Bernoulli', sum(bernoulli_accuracy)/len(bernoulli_accuracy)))
+    models_accuracy.append(('Random', sum(random_accuracy)/len(random_accuracy)))
 
-    y_pred = train_naive_bayes(x_train, y_train, x_test)
-    accuracy =test_naive_bayes(y_test, y_pred)
-    models_accuracy.append(('Naive Bayes', accuracy))
-
-    y_pred = train_logistic_regression(x_train, y_train, x_test)
-    accuracy = test_logistic_regression(y_test, y_pred)
-    models_accuracy.append(('Logistic Regression', accuracy))
-
-    y_pred = train_support_vector(x_train, y_train, x_test)
-    accuracy = test_support_vector(y_test, y_pred)
-    models_accuracy.append(('Support Vectors', accuracy))
-
-    y_pred = train_bernoulli(x_train, y_train, x_test)
-    accuracy = test_bernoulli(y_test, y_pred)
-    models_accuracy.append(('Bernoulli', accuracy))
-
-    y_pred = train_random()
-    accuracy = test_random(y_test, y_pred)
-    models_accuracy.append(('Random', accuracy))
-
+    #Plot the results of the average of each model
     plot_results(models_accuracy)
 
 if __name__ == "__main__":
+    parse_args()
     main()
